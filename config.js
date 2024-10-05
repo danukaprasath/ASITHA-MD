483cf1ebcfc81bde676a1e8f3e06340a6c15589f
danukaprasath <dhanukadhanuka57@gmail.com>
2024-10-05 18:07:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=3I10QBQS#Y-cgww1TeYVXz1zYkXomsN0GOO5fjo_TuMqSrhYoIHs",
+MONGODB: process.env.MONGODB |94772484733| "",//enter mongo db url
 };
